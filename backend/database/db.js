6d2e552845bdffaa3f6bee692b58f2a9b206da4c
tonyo-psijas <appsijasig@gmail.com>
2026-03-12 20:16:54 -0300

    fix env

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,10 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config()
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    connectionString: process.env.DATABASE_URL,
     allowExitOnIdle: true
 })
 
--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,3 +1,3 @@
 create database ejercicios;
 --\c ejercicios
-CREATE TABLE items (name TEXT, price INT);
+CREATE TABLE items (name TEXT, price INT, id SERIAL PRIMARY KEY);
